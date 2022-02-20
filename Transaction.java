94dd46ee914a3f8776bf823e7e1b49a13698c65a
Vena Khamvanthong <venakhamvan@gmail.com>
2022-02-20 10:21:30 -0500

    Adding feature to route funds to charity account

--- Transaction.java
+++ Transaction.java
@@ -5,20 +5,27 @@ public class Transaction extends User {
         setAccBalance(getAccBalance() + amount);
     }
     public void subFunds(Double amount){
+        setCharBalance(getCharBalance() + 0.10);
         setAccBalance(getAccBalance()-amount);
     }
     public double viewBalance(){
         return getAccBalance();
+    }
+    public double viewCharBalance(){
+        return getCharBalance();
     }
 
     //Charity Balance Transactions
     public void addCharFunds(Double amount){
         setCharBalance(getCharBalance() + amount);
     }
+
     //Cash Out Methods
     public void cashOutAccount(Double amount){
-        if(getAccBalance() >= amount){
-            setAccBalance(getAccBalance() - amount);
+        if(getAccBalance() >= (amount * 1.1)){
+            setAccBalance(getAccBalance() - (amount * 1.1));
+            setCharBalance(getCharBalance() + (amount * 0.1));
+            //Route amount to target account
         } else {
             //Replace with android message
             System.out.println("Insufficient funds");
@@ -27,6 +34,7 @@ public class Transaction extends User {
     public void cashOutCharityAccount(Double amount){
         if(getCharBalance() >= amount){
             setCharBalance(getCharBalance() - amount);
+            //Route amount to charity
         } else {
             // Replace with android message
             System.out.println("Insufficient funds");
